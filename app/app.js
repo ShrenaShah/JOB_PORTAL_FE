441bbce14dae95829b7796edf512f179e9694ed5
ShrenaShah <shrenashah7@gmail.com>
2025-06-20 23:00:45 +0530

    adding save job features

--- app/app.js
+++ app/app.js
@@ -1,47 +1,53 @@
 // app/app.js
 const app = angular.module("jobPortalApp", ["ngRoute"]);
 
-app.config(["$routeProvider", function($routeProvider) {
+app.config([
+  "$routeProvider",
+  function ($routeProvider) {
     $routeProvider
-        .when("/login", {
-            templateUrl: "app/views/login.html",
-            controller: "LoginController"
-        })
-        .when("/register", {
-            templateUrl: "app/views/register.html",
-            controller: "RegisterController"
-        })
-        .when("/jobs", {
-            templateUrl: "app/views/jobs.html",
-            controller: "JobsController"
-        })
-        .when("/create-job", {
-            templateUrl: "app/views/createJob.html",
-            controller: "CreateJobController"
-        })
-        .when("/profile", {
-            templateUrl: "app/views/profile.html",
-            controller: "ProfileController"
-        })
-        .when("/applications/:jobId", {
-            templateUrl: "app/views/viewApplications.html",
-            controller: "ViewApplicationController"
-        })
-        .otherwise({
-            redirectTo: "/login"
-        });
-}]);
+      .when("/login", {
+        templateUrl: "app/views/login.html",
+        controller: "LoginController",
+      })
+      .when("/register", {
+        templateUrl: "app/views/register.html",
+        controller: "RegisterController",
+      })
+      .when("/jobs", {
+        templateUrl: "app/views/jobs.html",
+        controller: "JobsController",
+      })
+      .when("/create-job", {
+        templateUrl: "app/views/createJob.html",
+        controller: "CreateJobController",
+      })
+      .when("/profile", {
+        templateUrl: "app/views/profile.html",
+        controller: "ProfileController",
+      })
+      .when("/applications/:jobId", {
+        templateUrl: "app/views/viewApplications.html",
+        controller: "ViewApplicationController",
+      })
+      .otherwise({
+        redirectTo: "/login",
+      });
+  },
+]);
 
-app.controller("MainController", ["$scope", function($scope) {
-    $scope.baseUrl = "https://job-portal-be-jwgm.onrender.com/api";
+app.controller("MainController", [
+  "$scope",
+  function ($scope) {
+    $scope.baseUrl = "https://job-portal-be-mbyo.onrender.com/api";
 
-    $scope.logout = function() {
-        document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        window.location.href = "/#!/login";
+    $scope.logout = function () {
+      document.cookie =
+        "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+      window.location.href = "/#!/login";
     };
-    $scope.getLoggedInUser = function() {
-        const user = localStorage.getItem("user");
-        return user ? JSON.parse(user) : null;
+    $scope.getLoggedInUser = function () {
+      const user = localStorage.getItem("user");
+      return user ? JSON.parse(user) : null;
     };
     // $scope.isEmployer = function() {
     //     const user = $scope.getLoggedInUser();
@@ -50,20 +56,24 @@ app.controller("MainController", ["$scope", function($scope) {
     //     }
     //     return false; // Explicitly return false if the user is not an employer
     // }
-}]);
+  },
+]);
 
-app.directive("fileModel", ["$parse", function($parse) {
+app.directive("fileModel", [
+  "$parse",
+  function ($parse) {
     return {
-        restrict: "A",
-        link: function(scope, element, attrs) {
-            const model = $parse(attrs.fileModel);
-            const modelSetter = model.assign;
+      restrict: "A",
+      link: function (scope, element, attrs) {
+        const model = $parse(attrs.fileModel);
+        const modelSetter = model.assign;
 
-            element.bind("change", function() {
-                scope.$apply(function() {
-                    modelSetter(scope, element[0].files[0]);
-                });
-            });
-        }
+        element.bind("change", function () {
+          scope.$apply(function () {
+            modelSetter(scope, element[0].files[0]);
+          });
+        });
+      },
     };
-}]);
+  },
+]);
